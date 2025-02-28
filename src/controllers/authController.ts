6f6fb7e1a93b56faa5b3b8edf3be2106dc0675bd
Typical-Techno <shubhamjangrartk@gmail.com>
2025-02-28 10:49:09 +0530

    added checktoken route

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -7,11 +7,17 @@ const SECRET = process.env.JWT_SECRET || 'supersecret';
 
 export const signup = async (req: Request, res: Response): Promise<void> => {
     try {
-        const { email, password, username, desigination, police_thana, phoneNumber } =
-            req.body;
+        const {
+            email,
+            password,
+            username,
+            desigination,
+            police_thana,
+            phoneNumber
+        } = req.body;
 
         const existingUser = await prisma.user.findUnique({
-            where: { email }
+            where: { email, phoneNumber }
         });
         if (existingUser) {
             res.status(400).json({ error: 'Username already exists' });
@@ -32,6 +38,7 @@ export const signup = async (req: Request, res: Response): Promise<void> => {
 
         res.status(201).json({ message: 'User created successfully' });
     } catch (error: any) {
+        console.log(error.message);
         res.status(500).json({ error: error.message });
     }
 };
@@ -53,8 +60,9 @@ export const login = async (req: Request, res: Response): Promise<void> => {
         }
 
         const token = jwt.sign({ userId: user.id }, SECRET, {
-            expiresIn: '1h'
+            expiresIn: '7d'
         });
+
         const userData = {
             id: user.id,
             username: user.username,
@@ -68,5 +76,21 @@ export const login = async (req: Request, res: Response): Promise<void> => {
         res.json({ userData, token });
     } catch (error: any) {
         res.status(500).json({ error: error.message });
+    }
+};
+
+export const checkToken = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token) {
+            res.status(401).json({ error: 'Token is required' });
+            return;
+        }
+
+        jwt.verify(token, SECRET);
+
+        res.json({ message: 'Token is valid' });
+    } catch (error: any) {
+        res.status(401).json({ error: 'Invalid or expired token' });
     }
 };
